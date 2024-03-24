ceed8a723bee58752ce3019016f6a7023eb67e13
SiddharthaSangani <156238859+SiddharthaSangani@users.noreply.github.com>
2024-03-24 09:28:17 -0500

    C4

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var denRouter = require('./routes/den');
 var gridRouter = require('./routes/grid');
+var randomRouter = require('./routes/pick');
+
 
 var app = express();
 
@@ -25,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/den', denRouter);
 app.use('/grid', gridRouter);
+app.use('/random',randomRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
