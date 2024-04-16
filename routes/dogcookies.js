2648fab724232ed3a078e08c0e8fb10988e23443
SiddharthaSangani <156238859+SiddharthaSangani@users.noreply.github.com>
2024-04-16 01:07:02 -0500

    f

--- routes/dogcookies.js
+++ routes/dogcookies.js
@@ -6,7 +6,7 @@ var router = express.Router();
 /* GET costumes */
 router.get('/detail', dogcookies_controlers.dogcookies_view_one_Page);
 router.get('/', dogcookies_controlers.dogcookies_view_all_Page);
-router.get('/create', dogcookies_controlers.dogcookies_create_Page);
+//router.get('/create', dogcookies_controlers.dogcookies_create_Page);
 //router.get('/update', dogcookies_controlers.dogcookies_update_Page);
 //router.get('/delete', dogcookies_controlers.dogcookies_delete_Page);
 //router.get('/update', dogcookies_controlers.dogcookies_update_Page);
